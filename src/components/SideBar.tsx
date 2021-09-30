c10eebe7a4dd4975be7509b82e674baec66f4e1f
Guilhermecheng <62719629+Guilhermecheng@users.noreply.github.com>
2021-09-29 23:55:02 -0300

    commit 2

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,40 @@
-export function SideBar() {
+import { Button } from "./Button";
+
+interface GenreProps {
+	genres: Array<{
+		id: number;
+		name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+		title: string;
+	}>;
+	genreId: number;
+	changeGenreId: (genreId: number) => number | void;
+}
+
+export function SideBar({ genres, genreId, changeGenreId }: GenreProps) {
   // Complete aqui
+  
+
+  function handleClickButton(id: number) {
+    // setSelectedGenreId(id);
+	changeGenreId(id);
+  }
+
+	return (
+		<nav className="sidebar">
+			<span>Watch<p>Me</p></span>
+
+			<div className="buttons-container">
+				{genres.map(genre => (
+				<Button
+					key={String(genre.id)}
+					title={genre.title}
+					iconName={genre.name}
+					onClick={() => handleClickButton(genre.id)}
+					// selected={selectedGenreId === genre.id}
+					selected={genreId === genre.id}
+				/>
+				))}
+			</div>
+		</nav>
+	)
 }
